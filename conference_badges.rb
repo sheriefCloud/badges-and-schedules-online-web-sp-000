dc8125e9cdc7c6a0e72d487768c5488d317d01f6
Sherief Farid <sherief@farid.cloud>
2019-03-02 06:40:35 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def badge_maker(name)
+  formated = "Hello, my name is #{name}."
+  return formated
+end
+
+def batch_badge_creator(attendees)
+  messages = []
+  attendees.each do |attendee|
+    message  = badge_maker(attendee)
+    messages << message
+  end
+  return messages
+end
+
+def assign_rooms(attendees)
+  rooms_assignment = []
+  room = 1
+  attendees.each do |attendee|
+    room_assign = "Hello, #{attendee}! You'll be assigned to room #{room}!"}
+    # room += 1
+    rooms_assignment << room_assign
+  end
+end
+
+def printer
+  attendees.each do |attendee_message|
+    puts
+
+  end
+end
